4c441b1ef2470ec6860bab22d2c3f9718bf867c6
Daniel Yavorovych <daniel@dysnix.com>
2020-09-07 13:12:43 +0300

    add logging

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # coding=utf-8
 import os
+import logging
 from ethernodes import Ethernodes
 
 PEERS_FILE_PATH = os.environ.get('PEERS_FILE_PATH', '/tmp/geth-nodes-list-builder/peers.txt')
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     en = Ethernodes()
     en.make_peers_file(PEERS_FILE_PATH)
+    logging.info('File {} created'.format(PEERS_FILE_PATH))
